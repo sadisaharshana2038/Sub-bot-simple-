8289bb629980743e40225978de3e2d0ad0731e27
sadisaharshana2038 <sadisaharshana2038@gmail.com>
2026-02-16 15:59:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ load_dotenv()
 # CONFIGURATION
 # ============================================
 
-BOT_TOKEN = os.getenv('BOT_TOKEN', '8564530553:AAGNoHoK5374qB_WfDhy12mo5jHazLk63xo')
+BOT_TOKEN = os.getenv('BOT_TOKEN', '8564530553:AAG49A0pAuR9qRqyd2FDnZyd85MfE2K8w4E')
 MONGODB_URI = os.getenv('MONGODB_URI', 'mongodb+srv://Sadisa:JRGgclOXbm5KLiHn@cluster0.vexxjgb.mongodb.net/')
 DB_NAME = os.getenv('DB_NAME', 'sinhala_sub_bot')
 ADMIN_IDS = [int(x) for x in os.getenv('ADMIN_IDS', '8107411538,7001801397').split(',') if x]
